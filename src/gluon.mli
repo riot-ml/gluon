819a41cc83783fcc84cf363ebbbd4173bb47f61e
Leandro Ostera <leandro@abstractmachines.dev>
2024-11-05 23:55:55 +0100

    fix: lift unix and block errors here

--- src/gluon.mli
+++ src/gluon.mli
@@ -1,5 +1,8 @@
 type io_error =
-  [ `Could_not_resolve_uri of Uri.t | `Uri_has_no_host of Uri.t | Rio.io_error ]
+  [ `Could_not_resolve_uri of Uri.t
+  | `Uri_has_no_host of Uri.t
+  | `Unix_error of Unix.error
+  | `Would_block ]
 
 type ('ok, 'err) io_result = ('ok, ([> io_error ] as 'err)) Stdlib.result
 
